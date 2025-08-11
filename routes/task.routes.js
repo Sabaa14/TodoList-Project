e7d916b791fb38be5f637971826b8a1f3109ec15
Abdelrahman Sabaa <s90487@bht-berlin.de>
2025-08-11 20:21:02 +0200

    finishing the middleware

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const  router = express.Router();
-const {readtask , readtaskbyId, createtask ,updatetask ,deletetask} = require("../controllers/task.controllers");
-
 
 router.get("/", readtask);
 router.get("/:id",readtaskbyId);
